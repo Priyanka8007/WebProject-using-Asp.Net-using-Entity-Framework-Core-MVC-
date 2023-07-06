87607ba36f2d83ec253db53b88a207112d3d8d59
Priyanka8007 <kadampriyanka1696@gmail.com>
2023-07-06 01:22:30 -0700

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using WebProject.Models;
+using Microsoft.EntityFrameworkCore;
+namespace WebProject.Data
+{
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Category21Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Category21Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Category21Id = 3, Name = "SciFi", DisplayOrder = 3 }
+                );
+
+        }
+    }
+}
+
---
+++ Migrations/20230618043736_SeedCategoryTable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "Category21Id");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Category21Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "History" },
+                    { 3, 3, "SciFi" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Category21Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Category21Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Category21Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Category21Id",
+                table: "Categories",
+                newName: "Id");
+        }
+    }
+}
